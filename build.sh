49b71714cb866ecc38b64d7b5512699a10c59c37
Matt Bornski <matt@bornski.com>
2016-01-21 08:26:23 -0800

    Can build and execute.  Doesn't do anything yet.

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mcs -r:System.ServiceModel -r:System.Runtime.Serialization -r:System.Web audio.cs
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+brew install mono
+brew install https://raw.githubusercontent.com/escaped/homebrew/2f1a0c6bc39c3f65f123098cd0c83aae3d5cca15/Library/Formula/libbass.rb
